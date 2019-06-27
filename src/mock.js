bc92383e49d1b8a5de43850246438093bc028e89
zhangjiawu <769535935@qq.com>
2019-06-27 10:42:37 +0800

    zje

--- src/mock.js
+++ src/mock.js
@@ -33,30 +33,6 @@ Mock.mock('/articleList', function () {
   return articleList
 })
 
-<<<<<<< HEAD
-  let textCategoryList ={
-      code:0,
-      message:"操作成功",
-      page:{
-      pageIndex: 1,
-      pageSize: 3,
-      allCount: 1,
-      pageCount: 1
-      },
-    list:[
-    {P1:1,name:"唐球规则",explin:"关于唐球的各项规则说明",articleNumber:"3",
-    article:[{title:"文章分类唐球规则下的文章一",text:"这是一篇文章"},
-    {title:"文章分类唐球规则下的文章二",text:"这是一篇文章"}, 
-    {title:"文章分类唐球规则下的文章三",text:"这是一篇文章"},]},
-    {P1:2,name:"比赛说明",explin:"关于唐球比赛的各项规则说明",articleNumber:"3",
-    article:[{title:"文章分类比赛说明下的文章一",text:"这是一篇文章"},
-    {title:"文章分类比赛说明下的文章二",text:"这是一篇文章"},
-    {title:"文章分类比赛说明下的文章三",text:"这是一篇文章"},]},
-    {P1:3,name:"推广赞助",explin:"关于唐球的推广赞助的说明",articleNumber:"3",
-    article:[{title:"文章分类推广赞助下的文章一",text:"这是一篇文章"},
-    {title:"文章分类推广赞助下的文章二",text:"这是一篇文章"},
-    {title:"文章分类推广赞助下的文章三",text:"这是一篇文章"},]},
-=======
 Mock.mock('/textCategory', function () {
   let textCategoryList = [
     {
@@ -77,7 +53,6 @@ Mock.mock('/textCategory', function () {
       { title: "文章分类推广赞助下的文章二", text: "这是一篇文章" },
       { title: "文章分类推广赞助下的文章三", text: "这是一篇文章" },]
     },
->>>>>>> 1ef915e14e9925bcb25ccba4c0e4e17510198c39
   ]
   return textCategoryList
 
@@ -130,38 +105,3 @@ Mock.mock('/list_enroll', function () {
 
 })
 //场馆接口
-Mock.mock('/list_venue',function(){
-  let list_venue={
-    "code": 0,
-    "message": "操作成功",
-    "page": {
-      "pageIndex": 1,
-      "pageSize": 5,
-      "allCount": 1,
-      "pageCount": 1
-    },
-    "list":[
-      {
-        "P1":1,
-        "name":"一号场馆",
-        "area":"A区",
-        "countMatch":"5",
-        // "album"
-        "time":"2016-6-10",
-        "phoneNumber":"18218024345"
-
-      },
-      {
-        "P1":2,
-        "name":"二号场馆",
-        "area":"B区",
-        "countMatch":"5",
-        // "album"
-        "time":"2016-6-11",
-        "phoneNumber":"13213125535"
-      },
-      
-    ]
-  }
-  return list_venue
-})
