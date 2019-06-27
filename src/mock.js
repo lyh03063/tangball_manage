644d5dcead723c636299c1b5a5e04305115b2d44
Xiaoqilin <469158755@qq.com>
2019-06-27 09:34:52 +0800

    abccddd

--- src/mock.js
+++ src/mock.js
@@ -10,7 +10,13 @@ const userData = () => {
   return { a: 1 }
 }
 Mock.mock('/api/users', userData)
-
+Mock.mock('/login', function () {
+let loginList=[
+  {P1:1,userName:"admin",passWord:"aa"},
+  {P1:2,userName:"admin",passWord:"aa"},
+]
+return loginList
+})
 
 Mock.mock('/articleList', function () {
   let articleList = [
@@ -41,7 +47,7 @@ Mock.mock('/listArticleCategory', function(){
     list:[
     {P1:1,name:"唐球规则",explin:"关于唐球的各项规则说明",articleNumber:"3",
     article:[{title:"文章分类唐球规则下的文章一",text:"这是一篇文章"},
-    {title:"文章分类唐球规则下的文章二",text:"这是一篇文章"},
+    {title:"文章分类唐球规则下的文章二",text:"这是一篇文章"}, 
     {title:"文章分类唐球规则下的文章三",text:"这是一篇文章"},]},
     {P1:2,name:"比赛说明",explin:"关于唐球比赛的各项规则说明",articleNumber:"3",
     article:[{title:"文章分类比赛说明下的文章一",text:"这是一篇文章"},
