47d8e2c63d20384ef9edb72128a136c2b71510af
lyh <113381161@qq.com>
2019-07-01 16:31:01 +0800

    lyh

--- src/page/list_lyh.vue
+++ src/page/list_lyh.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class>
-    <ajax_populate :id="id" populateKey="name"></ajax_populate>
-
-    <ajax_populate :id="id" >
-      <template v-slot:default="{doc}">{{doc}}</template>
-    </ajax_populate>
-  </div>
+  <div class>{{arrProvice}}</div>
 </template>
 <script>
 //导入自定义复选框组件，注意路径改为自己的
@@ -15,8 +9,60 @@ export default {
   components: { ajax_populate },
   data() {
     return {
-      id: 1
+      arrProvice: null
     };
+  },
+  created() {
+    let arrDataBase = [
+      {
+        P2: "广西",
+        P7: "45",
+        P8: "0001"
+      },
+      {
+        P2: "广东",
+        P7: "44",
+        P8: "0001"
+      },
+      {
+        P2: "深圳",
+        P7: "4403",
+        P8: "44"
+      },
+      {
+        P2: "韶关",
+        P7: "4402",
+        P8: "44"
+      },
+      {
+        P2: "南山区",
+        P7: "440305",
+        P8: "4403"
+      },
+      {
+        P2: "福田区",
+        P7: "440304",
+        P8: "4403"
+      }
+    ];
+    //变量：{省份地区数组}----使用filter过滤
+    this.arrProvice = arrDataBase.filter(docBase => docBase.P8 == "0001");
+    //函数定义：{获取子数据函数}
+    function getChildren(arr) {
+      console.count("getChildren");
+      arr.forEach(docEach => {
+        //使用filter过滤出当前地区的子地区数组
+        docEach.children = arrDataBase.filter(
+          docBase => docBase.P8 == docEach.P7
+        );
+        if (docEach.children && docEach.children.length) {
+          //如果子地区数组存在
+          getChildren(docEach.children); //递归调用：{获取子数据函数}
+        }
+      });
+    }
+    
+    getChildren(this.arrProvice); //首次调用：{获取子数据函数}
   }
 };
 </script>
