0f6d6bdbb73f097331a9becf603c7fd7fd4dd5b5
lyh <113381161@qq.com>
2019-07-02 16:09:55 +0800

    lyh

---
+++ src/components/form_item/select_city.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class>
+    <el-cascader
+      :options="options"
+      @active-item-change="handleItemChange"
+      @change="changeArea"
+      :props="props"
+      v-model="valueNeed"
+    ></el-cascader>
+  </div>
+</template>
+
+<script>
+
+/** 这个组件比较坑的地方在于其默认绑定的value数据格式是编号数组形式
+ * 获取城市名称很不方便
+ * 
+*/
+export default {
+  props: {
+    value: {
+      type: Array,
+      default: function() {
+        return ["44", "4401"];
+      }
+    },
+    cityName: {
+      type: String,
+    }
+  },
+  data() {
+    return {
+      url: {
+        list: "http://120.76.160.41:3000/crossList?page=dmagic_area" //列表接口
+      },
+      valueNeed: this.value,
+      options: [
+        {
+          value: "44",
+          label: "广东",
+          cities: [
+            {
+              value: "4401",
+              label: "aaas"
+            }
+          ]
+        },
+        {
+          value: "45",
+          label: "广西",
+          cities: []
+        }
+      ],
+      cityList: null, //缓存的城市文档列表，用于获取城市名称
+      props: {
+        // value: "label",
+        children: "cities" //指定children键名
+      }
+    };
+  },
+  watch: {
+    valueNeed: {
+      handler(newName, oldName) {
+        this.$emit("input", this.valueNeed); //同步valueNeed值到value
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+
+  methods: {
+    changeArea(arr) {
+      console.log("arr####", arr);
+
+      //这里如何获取到cityName
+      let cityId = arr[1];
+      console.log("cityId", cityId);
+      console.log("this.cityList", this.cityList);
+      let docCity = this.cityList.find(doc => doc.P7 == cityId);
+      console.log("docCity", docCity);
+      let cityName = docCity.P2;
+      console.log("cityName##########", cityName);
+      this.$emit("change-city-name", cityName); //同步valueNeed值到value
+
+    },
+    async ajaxGetOp(pid) {
+      //请求接口
+      let { data } = await axios({
+        method: "post",
+        url: this.url.list,
+        //传递参数
+        data: {
+          findJson: { P8: pid }
+        }
+      });
+      if (pid != "0001") {
+        //如果不是获取一级数据
+        this.cityList = data.list; //缓存城市列表
+      }
+
+      return data.list.map(docEach => {
+        return {
+          value: docEach.P7,
+          label: docEach.P2,
+          cities: pid == "0001" ? [] : null //如果是省份数据，给个空数组
+        };
+      });
+    },
+    async handleItemChange(val, p2) {
+      console.log("active item:", val);
+      console.log("p2", p2);
+
+      let provinceId = val[0];
+      let objOption = this.options.find(opEach => opEach.value == provinceId);
+      objOption.cities = await this.ajaxGetOp(provinceId);
+    }
+  },
+  async created() {
+    this.options = await this.ajaxGetOp("0001");
+
+    if (this.valueNeed && this.valueNeed[0]) {
+      let provinceId = this.valueNeed[0];
+      let objOption = this.options.find(opEach => opEach.value == provinceId);
+      objOption.cities = await this.ajaxGetOp(provinceId);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/page/list_lyh.vue
+++ src/page/list_lyh.vue
@@ -1,76 +1,26 @@
 <template>
-  <div class>{{arrProvice}}</div>
+  <div class>
+   绑定数据 {{objCityVanue}}
+    <select_city_venue v-model="objCityVanue"></select_city_venue>
+  </div>
 </template>
 <script>
-//导入自定义复选框组件，注意路径改为自己的
-import ajax_populate from "../components/common/ajax_populate.vue";
+
+import select_city_venue from "../components/form_item/select_city_venue.vue";
 
 export default {
-  components: { ajax_populate },
+  components: { select_city_venue },
   data() {
     return {
-      arrProvice: null
+      objCityVanue:{"cityId":"4401","cityName":"广州","venueId":15,"venueName":"场馆名称"}
     };
   },
-  created() {
-    let arrDataBase = [
-      {
-        P2: "广西",
-        P7: "45",
-        P8: "0001"
-      },
-      {
-        P2: "广东",
-        P7: "44",
-        P8: "0001"
-      },
-      {
-        P2: "深圳",
-        P7: "4403",
-        P8: "44"
-      },
-      {
-        P2: "韶关",
-        P7: "4402",
-        P8: "44"
-      },
-      {
-        P2: "南山区",
-        P7: "440305",
-        P8: "4403"
-      },
-      {
-        P2: "福田区",
-        P7: "440304",
-        P8: "4403"
-      }
-    ];
-    //变量：{省份地区数组}----使用filter过滤
-    this.arrProvice = arrDataBase.filter(docBase => docBase.P8 == "0001");
-    //函数定义：{获取子数据函数}
-    function getChildren(arr) {
-      console.count("getChildren");
-      arr.forEach(docEach => {
-        //使用filter过滤出当前地区的子地区数组
-        let arrChildren = arrDataBase.filter(
-          docBase => docBase.P8 == docEach.P7
-        );
-
-        //如果子地区数组存在
-        if (arrChildren.length) {
-          docEach.children = arrChildren;
-        }
-
-        if (docEach.children) {
-          //如果子地区数组存在
-          getChildren(docEach.children); //递归调用：{获取子数据函数}
-        }
-      });
-    }
-
-    getChildren(this.arrProvice); //首次调用：{获取子数据函数}
+  methods: {
+    
+  },
+  async created() {
+    //this.venueOp = await this.ajaxGetVenue("4401");
   }
 };
 </script>
-
 
