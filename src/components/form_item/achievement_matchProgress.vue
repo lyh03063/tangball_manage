6574c7b911a890640fdd4800304f00e3c3f3d987
曾楚杰 <2293354735@qq.com>
2019-07-06 15:27:54 +0800

    123456

--- src/components/form_item/achievement_matchProgress.vue
+++ src/components/form_item/achievement_matchProgress.vue
@@ -10,27 +10,37 @@ export default {
       nweMatchProgress: this.matchProgress
     };
   },
-  methods: {},
-  mounted() {
-    console.log("matchProgress", this.nweMatchProgress);
-
-    if (this.nweMatchProgress.bigProgress == 1) {
-      if (this.nweMatchProgress.smallProgress == 11) {
-        this.nweMatchProgress = "城市赛：选拔赛";
-      } else if (this.nweMatchProgress.smallProgress == 12) {
-        this.nweMatchProgress = "城市赛：晋级赛";
+  watch: {
+    matchProgress: function() {
+      // alert(1)
+      this.matchProgressNeed()
+    }
+  },
+  methods: {
+    matchProgressNeed() {
+      this.nweMatchProgress= this.matchProgress;
+      if (this.nweMatchProgress.bigProgress == 1) {
+        if (this.nweMatchProgress.smallProgress == 11) {
+          this.nweMatchProgress = "城市赛：选拔赛";
+        } else if (this.nweMatchProgress.smallProgress == 12) {
+          this.nweMatchProgress = "城市赛：晋级赛";
+        } else {
+          this.nweMatchProgress = "城市赛：决赛";
+        }
       } else {
-        this.nweMatchProgress = "城市赛：决赛";
-      }
-    } else {
-      if (this.nweMatchProgress.smallProgress == 21) {
-        this.nweMatchProgress = "城际赛：淘汰赛/循环赛";
-      } else if (this.nweMatchProgress.smallProgress == 22) {
-        this.nweMatchProgress = "城际赛：1/4决赛";
-      } else {
-        this.nweMatchProgress = "城际赛：决赛";
+        if (this.nweMatchProgress.smallProgress == 21) {
+          this.nweMatchProgress = "城际赛：淘汰赛/循环赛";
+        } else if (this.nweMatchProgress.smallProgress == 22) {
+          this.nweMatchProgress = "城际赛：1/4决赛";
+        } else {
+          this.nweMatchProgress = "城际赛：决赛";
+        }
       }
     }
+  },
+  mounted() {
+    console.log("matchProgress", this.nweMatchProgress);
+    this.matchProgressNeed()
   }
 };
 </script>
