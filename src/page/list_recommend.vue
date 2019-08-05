e72d8211dc9419e69b85971baccb1e0533218a95
lyh <113381161@qq.com>
2019-08-05 21:37:12 +0800

    lyh

--- src/page/list_recommend.vue
+++ src/page/list_recommend.vue
@@ -1,15 +1,27 @@
 <template>
   <div class>
     <listData :cf="cfList">
-     
+      <template v-slot:slot_detail_item_album="{row}">
+        <openPhoto v-model="row.album"></openPhoto>
+        <!-- <div>
+          <img
+            @click.stop="showBigImg(item.url)"
+            :src="item.url"
+            alt
+            v-for="item in row.album"
+            :key="item.url"
+            class="W100 H100"
+          >
+        </div>-->
+      </template>
     </listData>
   </div>
 </template>
 <script>
 import listData from "@/components/list-data/list-data.vue";
-
+import openPhoto from "@/components/common/openPhoto/openPhoto.vue";
 export default {
-  components: { listData },
+  components: { listData, openPhoto },
   data() {
     return {
       cfList: {
@@ -17,7 +29,7 @@ export default {
         focusMenu: true, //进行菜单聚焦
         twoTitle: "其他数据",
         threeTitle: "首页轮播图",
-       
+
         url: {
           list: "/crossList?page=tangball_recommend", //列表接口
           add: "/crossAdd?page=tangball_recommend", //新增接口
@@ -25,11 +37,18 @@ export default {
           detail: "/crossDetail?page=tangball_recommend",
           delete: "/crossDelete?page=tangball_recommend" //删除接口
         },
-        dynamicDict: [{ page: "tangball_article_category",populateColumn: "categoryDoc", idColumn: "articleCategory", idColumn2: "P1" }],
+        dynamicDict: [
+          {
+            page: "tangball_article_category",
+            populateColumn: "categoryDoc",
+            idColumn: "articleCategory",
+            idColumn2: "P1"
+          }
+        ],
 
         //-------列配置数组-------
         columns: [
-       {
+          {
             label: "标题",
             prop: "name",
             width: 260
@@ -39,12 +58,11 @@ export default {
             prop: "link",
             width: 250
           },
-          
+
           {
             label: "创建时间",
             prop: "CreateTime",
-            width: 145,
-           
+            width: 145
           },
           {
             label: "其他",
@@ -57,41 +75,44 @@ export default {
         ],
         //-------筛选表单字段数组-------
         searchFormItems: [
-          
           {
             label: "标题",
             prop: "name",
             type: "input_find_vague"
           }
-          
         ],
         //-------详情字段数组-------
         detailItems: [
           {
             label: "标题",
-            prop: "name",
+            prop: "name"
           },
           {
             label: "链接页面",
-            prop: "link",
+            prop: "link"
           },
+
+          {
+            label: "图片",
+            prop: "album",
+            slot: "slot_detail_item_album"
+          }
         ],
         //-------新增、修改表单字段数组-------
         formItems: [
-         
           {
             label: "文章标题",
-            prop: "name",
+            prop: "name"
           },
           {
             label: "小程序页面",
-            prop: "link",
+            prop: "link"
           },
           {
             label: "相册",
             prop: "album",
-            type: "editor"
-          }
+            type: "upload"
+          },
         ]
       }
     };
